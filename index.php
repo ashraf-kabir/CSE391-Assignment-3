6ec08697791d57dc692912afa536650931f0583d
Ashraf Kabir <ashrafkabir95@gmail.com>
2019-10-18 13:53:44 +0600

    changes

--- index.php
+++ index.php
@@ -51,6 +51,50 @@ if (strlen($_SESSION['login']) == 0) {
                     ?>
                 </div>
 
+                <!-- Group Name -->
+                <div class="col-md-9">
+                    <?php
+                    $email = $_SESSION['login'];
+                    $sql = "SELECT `sgroup` FROM `users` WHERE `email`=:email";
+                    $query = $dbh->prepare($sql);
+                    $query->bindParam(':email', $email, PDO::PARAM_STR);
+                    $query->execute();
+                    $results = $query->fetchAll(PDO::FETCH_OBJ);
+                    $cnt = 1;
+                    if ($query->rowCount() > 0) {
+                        foreach ($results
+                                 as $result) {
+                            echo "<br><h2>Group $result->sgroup</h2>";
+                            $rC = $query->rowCount();
+                            echo "<br><h2>total rows: $rC</h2>";
+                        }
+                    }
+                    ?>
+                </div>
+
+                <!-- Row Count -->
+                <div class="col-md-9">
+                    <?php
+                    $email = $_SESSION['login'];
+                    $sgroup = "g1";
+                    $sql = "SELECT `sgroup` FROM `users` WHERE `email`=:email AND `sgroup`=:sgroup";
+                    $query = $dbh->prepare($sql);
+                    $query->bindParam(':email', $email, PDO::PARAM_STR);
+                    $query->bindParam(':sgroup', $sgroup, PDO::PARAM_STR);
+                    $query->execute();
+                    $results = $query->fetchAll(PDO::FETCH_OBJ);
+                    $cnt = 1;
+                    if ($query->rowCount() > 0) {
+                        foreach ($results
+                                 as $result) {
+                            echo "<br><h2>Group $result->sgroup</h2>";
+                            $rC = $query->rowCount();
+                            echo "<br><h2>total rows: $rC</h2>";
+                        }
+                    }
+                    ?>
+                </div>
+
                 <!-- Sidebar Widgets Column -->
                 <?php include 'includes/sidebar.php'; ?>
             </div>
