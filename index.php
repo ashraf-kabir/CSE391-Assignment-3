094596989ea6f2b1bc5360331f544a3244bc20d6
Ashraf Kabir <ashrafkabir95@gmail.com>
2019-10-18 14:29:42 +0600

    changes

--- index.php
+++ index.php
@@ -54,19 +54,83 @@ if (strlen($_SESSION['login']) == 0) {
                 <!-- Group Name -->
                 <div class="col-md-9">
                     <?php
-                    $email = $_SESSION['login'];
-                    $sql = "SELECT `sgroup` FROM `users` WHERE `email`=:email";
-                    $query = $dbh->prepare($sql);
-                    $query->bindParam(':email', $email, PDO::PARAM_STR);
+                    //$email = $_SESSION['login'];
+                    $sgroup = "g1";
+                    $sql1 = "SELECT `sgroup` FROM `users` WHERE `sgroup`=:sgroup";
+                    $query = $dbh->prepare($sql1);
+                    //$query->bindParam(':email', $email, PDO::PARAM_STR);
+                    $query->bindParam(':sgroup', $sgroup, PDO::PARAM_STR);
                     $query->execute();
                     $results = $query->fetchAll(PDO::FETCH_OBJ);
                     $cnt = 1;
                     if ($query->rowCount() > 0) {
                         foreach ($results
                                  as $result) {
-                            echo "<br><h2>Group $result->sgroup</h2>";
                             $rC = $query->rowCount();
-                            echo "<br><h2>total rows: $rC</h2>";
+                            echo "<br><h3>Group $result->sgroup, total rows: $rC</h3>";
+                        }
+                    }
+                    ?>
+                </div>
+
+                <div class="col-md-9">
+                    <?php
+                    //$email = $_SESSION['login'];
+                    $sgroup = "g2";
+                    $sql2 = "SELECT `sgroup` FROM `users` WHERE `sgroup`=:sgroup";
+                    $query = $dbh->prepare($sql2);
+                    //$query->bindParam(':email', $email, PDO::PARAM_STR);
+                    $query->bindParam(':sgroup', $sgroup, PDO::PARAM_STR);
+                    $query->execute();
+                    $results = $query->fetchAll(PDO::FETCH_OBJ);
+                    $cnt = 1;
+                    if ($query->rowCount() > 0) {
+                        foreach ($results
+                                 as $result) {
+                            $rC = $query->rowCount();
+                            echo "<br><h3>Group $result->sgroup, total rows: $rC</h3>";
+                        }
+                    }
+                    ?>
+                </div>
+
+                <div class="col-md-9">
+                    <?php
+                    //$email = $_SESSION['login'];
+                    $sgroup = "g3";
+                    $sql2 = "SELECT `sgroup` FROM `users` WHERE `sgroup`=:sgroup";
+                    $query = $dbh->prepare($sql2);
+                    //$query->bindParam(':email', $email, PDO::PARAM_STR);
+                    $query->bindParam(':sgroup', $sgroup, PDO::PARAM_STR);
+                    $query->execute();
+                    $results = $query->fetchAll(PDO::FETCH_OBJ);
+                    $cnt = 1;
+                    if ($query->rowCount() > 0) {
+                        foreach ($results
+                                 as $result) {
+                            $rC = $query->rowCount();
+                            echo "<br><h3>Group $result->sgroup, total rows: $rC</h3>";
+                        }
+                    }
+                    ?>
+                </div>
+
+                <div class="col-md-9">
+                    <?php
+                    //$email = $_SESSION['login'];
+                    $sgroup = "g4";
+                    $sql2 = "SELECT `sgroup` FROM `users` WHERE `sgroup`=:sgroup";
+                    $query = $dbh->prepare($sql2);
+                    //$query->bindParam(':email', $email, PDO::PARAM_STR);
+                    $query->bindParam(':sgroup', $sgroup, PDO::PARAM_STR);
+                    $query->execute();
+                    $results = $query->fetchAll(PDO::FETCH_OBJ);
+                    $cnt = 1;
+                    if ($query->rowCount() > 0) {
+                        foreach ($results
+                                 as $result) {
+                            $rC = $query->rowCount();
+                            echo "<br><h3>Group $result->sgroup, total rows: $rC</h3>";
                         }
                     }
                     ?>
@@ -75,25 +139,20 @@ if (strlen($_SESSION['login']) == 0) {
                 <!-- Row Count -->
                 <div class="col-md-9">
                     <?php
-                    $email = $_SESSION['login'];
-                    $sgroup = "g1";
-                    $sql = "SELECT `sgroup` FROM `users` WHERE `email`=:email AND `sgroup`=:sgroup";
+                    //$email = $_SESSION['login'];
+                    $sql = "SELECT `sgroup` FROM `users`";
                     $query = $dbh->prepare($sql);
-                    $query->bindParam(':email', $email, PDO::PARAM_STR);
-                    $query->bindParam(':sgroup', $sgroup, PDO::PARAM_STR);
                     $query->execute();
                     $results = $query->fetchAll(PDO::FETCH_OBJ);
                     $cnt = 1;
                     if ($query->rowCount() > 0) {
-                        foreach ($results
-                                 as $result) {
-                            echo "<br><h2>Group $result->sgroup</h2>";
-                            $rC = $query->rowCount();
-                            echo "<br><h2>total rows: $rC</h2>";
-                        }
+                        $rC = $query->rowCount();
+                        echo "<br><h2>total rows: $rC</h2>";
                     }
                     ?>
                 </div>
+
+
 
                 <!-- Sidebar Widgets Column -->
                 <?php include 'includes/sidebar.php'; ?>
