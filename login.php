8645cbd23e31483d96c42cba216e21ddc12ac96e
Ashraf Kabir <ashrafkabir95@gmail.com>
2019-10-18 12:41:28 +0600

    changes

--- login.php
+++ login.php
@@ -4,10 +4,25 @@ include('includes/config.php');
 if (isset($_POST['login'])) {
     $email = $_POST['email'];
     $password = md5($_POST['password']);
-    $sql = "SELECT email,password FROM `groupa` WHERE email=:email and password=:password";
-    $query = $dbh->prepare($sql);
+    $sgroup = $_POST["sgroup"];
+
+    if ($sgroup == "g1") {
+        $sql1 = "SELECT email,password,sgroup FROM `groupa` WHERE email=:email AND password=:password";
+        $query = $dbh->prepare($sql1);
+    } elseif ($sgroup == "g2") {
+        $sql2 = "SELECT email,password,sgroup FROM `groupb` WHERE email=:email, password=:password AND sgroup=:sgroup";
+        $query = $dbh->prepare($sql2);
+    } elseif ($sgroup == "g3") {
+        $sql3 = "SELECT email,password,sgroup FROM `groupc` WHERE email=:email, password=:password AND sgroup=:sgroup";
+        $query = $dbh->prepare($sql3);
+    } elseif ($sgroup == "g4") {
+        $sql4 = "SELECT email,password,sgroup FROM `groupd` WHERE email=:email, password=:password AND sgroup=:sgroup";
+        $query = $dbh->prepare($sql4);
+    }
+
     $query->bindParam(':email', $email, PDO::PARAM_STR);
     $query->bindParam(':password', $password, PDO::PARAM_STR);
+    $query->bindParam(':sgroup', $sgroup, PDO::PARAM_STR);
     $query->execute();
     $results = $query->fetchAll(PDO::FETCH_OBJ);
 
@@ -69,7 +84,7 @@ if (isset($_POST['login'])) {
                         <div class="form-group ">
                             <div class="col-md-7">
                                 <input class="form-control" type="email" required="" name="email"
-                                       placeholder="Email" autocomplete="off">
+                                       placeholder="Email" autocomplete="on">
                             </div>
                         </div>
 
@@ -77,6 +92,17 @@ if (isset($_POST['login'])) {
                             <div class="col-md-7">
                                 <input class="form-control" type="password" name="password" required=""
                                        placeholder="Password" autocomplete="off">
+                            </div>
+                        </div>
+
+                        <div class="form-group">
+                            <div class="col-md-7">
+                                <select name="sgroup" id="sgroup">
+                                    <option value="g1">Group A</option>
+                                    <option value="g2">Group B</option>
+                                    <option value="g3">Group C</option>
+                                    <option value="g4">Group D</option>
+                                </select>
                             </div>
                         </div>
 
