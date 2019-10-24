67d9bf55bbcd665a2a93244542ecb91d582eab23
ashraf-kabir <ashrafkabir95@gmail.com>
2019-10-24 16:55:03 +0600

    changes

--- includes/functions.php
+++ includes/functions.php
@@ -19,4 +19,78 @@ function intro($dbh) {
     }
 }
 
+function slot1($dbh) {
+    $slot = "1";
+    $sql = "SELECT `slot` FROM `users` WHERE slot=:slot";
+    $query = $dbh->prepare($sql);
+    $query->bindParam(':slot', $slot, PDO::PARAM_STR);
+    $query->execute();
+    $results = $query->fetchAll(PDO::FETCH_OBJ);
+    global $rC1;
+    $rC1 = $query->rowCount();
+    global $rem1;
+    if ($rC1 > 0) {
+        $rem1 = 40 - $rC1;
+        echo "<h5>Slot 1 total rows: $rC1</h5>";
+    } else {
+        echo "<h5>Slot 1 total rows: 0</h5>";
+    }
+}
+
+function slot2($dbh) {
+    $slot = "2";
+    $sql = "SELECT `slot` FROM `users` WHERE slot=:slot";
+    $query = $dbh->prepare($sql);
+    $query->bindParam(':slot', $slot, PDO::PARAM_STR);
+    $query->execute();
+    $results = $query->fetchAll(PDO::FETCH_OBJ);
+    global $rC2;
+    $rC2 = $query->rowCount();
+    global $rem2;
+    if ($rC2 > 0) {
+        $rem2 = 40 - $rC2;
+        echo "<h5>Slot 2 total rows: $rC2</h5>";
+    } else {
+        echo "<h5>Slot 2 total rows: 0</h5>";
+    }
+}
+
+function slot3($dbh) {
+    $slot = "3";
+    $sql = "SELECT `slot` FROM `users` WHERE slot=:slot";
+    $query = $dbh->prepare($sql);
+    $query->bindParam(':slot', $slot, PDO::PARAM_STR);
+    $query->execute();
+    $results = $query->fetchAll(PDO::FETCH_OBJ);
+    global $rC3;
+    $rC3 = $query->rowCount();
+    global $rem3;
+    if ($rC3 > 0) {
+        $rem3 = 40 - $rC3;
+        echo "<h5>Slot 3 total rows: $rC3</h5>";
+    } else {
+        echo "<h5>Slot 3 total rows: 0</h5>";
+    }
+}
+
+function slot4($dbh) {
+    $slot = "4";
+    $sql = "SELECT `slot` FROM `users` WHERE slot=:slot";
+    $query = $dbh->prepare($sql);
+    $query->bindParam(':slot', $slot, PDO::PARAM_STR);
+    $query->execute();
+    $results = $query->fetchAll(PDO::FETCH_OBJ);
+    global $rC4;
+    $rC4 = $query->rowCount();
+    global $rem4;
+    if ($rC4 > 0) {
+        $rem4 = 40 - $rC4;
+        echo "<h5>Slot 4 total rows: $rC4</h5>";
+    } else {
+        echo "<h5>Slot 4 total rows: 0</h5>";
+    }
+}
+
+
+
 ?>
--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 session_start();
 error_reporting(0);
 include('includes/config.php');
+include('includes/functions.php');
 if (strlen($_SESSION['login']) == 0) {
     header("location: login.php");
 } else {
@@ -70,108 +71,27 @@ if (strlen($_SESSION['login']) == 0) {
 
                 <!-- Blog Entries Column -->
                 <div class="col-md-9">
-                    <?php
-                    $email = $_SESSION['login'];
-                    $sql = "SELECT `fname`,`lname` FROM `users` WHERE `email`=:email";
-                    $query = $dbh->prepare($sql);
-                    $query->bindParam(':email', $email, PDO::PARAM_STR);
-                    $query->execute();
-                    $results = $query->fetchAll(PDO::FETCH_OBJ);
-                    $cnt = 1;
-                    if ($query->rowCount() > 0) {
-                        foreach ($results
-                                 as $result) {
-                            $fname = $result->fname;
-                            $lname = $result->lname;
-                            $name = $fname . " " . $lname;
-                            echo "<br><h1>Log in successful $name</h1>";
-                        }
-                    }
-                    ?>
+                    <?php intro($dbh); ?>
                 </div>
 
                 <!-- Slot1 Row Count -->
                 <div class="col-md-9">
-                    <?php
-                    $slot = "1";
-                    $sql = "SELECT `slot` FROM `users` WHERE slot=:slot";
-                    $query = $dbh->prepare($sql);
-                    $query->bindParam(':slot', $slot, PDO::PARAM_STR);
-                    $query->execute();
-                    $results = $query->fetchAll(PDO::FETCH_OBJ);
-                    global $rC1;
-                    $rC1 = $query->rowCount();
-                    global $rem1;
-                    if ($rC1 > 0) {
-                        $rem1 = 40 - $rC1;
-                        echo "<h5>Slot 1 total rows: $rC1</h5>";
-                    } else {
-                        echo "<h5>Slot 1 total rows: 0</h5>";
-                    }
-                    ?>
+                    <?php slot1($dbh); ?>
                 </div>
 
                 <!-- Slot2 Row Count -->
                 <div class="col-md-9">
-                    <?php
-                    $slot = "2";
-                    $sql = "SELECT `slot` FROM `users` WHERE slot=:slot";
-                    $query = $dbh->prepare($sql);
-                    $query->bindParam(':slot', $slot, PDO::PARAM_STR);
-                    $query->execute();
-                    $results = $query->fetchAll(PDO::FETCH_OBJ);
-                    global $rC2;
-                    $rC2 = $query->rowCount();
-                    global $rem2;
-                    if ($rC2 > 0) {
-                        $rem2 = 40 - $rC2;
-                        echo "<h5>Slot 2 total rows: $rC2</h5>";
-                    } else {
-                        echo "<h5>Slot 2 total rows: 0</h5>";
-                    }
-                    ?>
+                    <?php slot2($dbh); ?>
                 </div>
 
                 <!-- Slot3 Row Count -->
                 <div class="col-md-9">
-                    <?php
-                    $slot = "3";
-                    $sql = "SELECT `slot` FROM `users` WHERE slot=:slot";
-                    $query = $dbh->prepare($sql);
-                    $query->bindParam(':slot', $slot, PDO::PARAM_STR);
-                    $query->execute();
-                    $results = $query->fetchAll(PDO::FETCH_OBJ);
-                    global $rC3;
-                    $rC3 = $query->rowCount();
-                    global $rem3;
-                    if ($rC3 > 0) {
-                        $rem3 = 40 - $rC3;
-                        echo "<h5>Slot 3 total rows: $rC3</h5>";
-                    } else {
-                        echo "<h5>Slot 3 total rows: 0</h5>";
-                    }
-                    ?>
+                    <?php slot3($dbh); ?>
                 </div>
 
                 <!-- Slot4 Row Count -->
                 <div class="col-md-9">
-                    <?php
-                    $slot = "4";
-                    $sql = "SELECT `slot` FROM `users` WHERE slot=:slot";
-                    $query = $dbh->prepare($sql);
-                    $query->bindParam(':slot', $slot, PDO::PARAM_STR);
-                    $query->execute();
-                    $results = $query->fetchAll(PDO::FETCH_OBJ);
-                    global $rC4;
-                    $rC4 = $query->rowCount();
-                    global $rem4;
-                    if ($rC4 > 0) {
-                        $rem4 = 40 - $rC4;
-                        echo "<h5>Slot 4 total rows: $rC4</h5>";
-                    } else {
-                        echo "<h5>Slot 4 total rows: 0</h5>";
-                    }
-                    ?>
+                    <?php slot4($dbh); ?>
                 </div>
 
 
@@ -269,7 +189,8 @@ if (strlen($_SESSION['login']) == 0) {
                     as $result) {
                     ?>
 
-                    <form class="form-horizontal" method="post" style="border-style: solid; padding-top: 20px; padding-left: 15px; background-color: #C8D3D5;">
+                    <form class="form-horizontal" method="post"
+                          style="border-style: solid; padding-top: 20px; padding-left: 15px; background-color: #C8D3D5;">
 
                         <div class="form-group">
                             <label class="col-md-2 control-label" for="input1">First Name</label>
@@ -306,7 +227,7 @@ if (strlen($_SESSION['login']) == 0) {
                             foreach ($results4 as $result4) {
                                 $gname = htmlentities($result4->name);
                                 $gdesc = htmlentities($result4->description);
-                                $gnamedesc = $gname.", ".$gdesc;
+                                $gnamedesc = $gname . ", " . $gdesc;
                                 ?>
                                 <div class="form-group">
                                     <label class="col-md-2 control-label" for="input4">Group</label>
@@ -405,7 +326,8 @@ if (strlen($_SESSION['login']) == 0) {
             </div>
             <!-- /.row -->
             <div class="row" style="margin-top: 25px;">
-                <div class="col-md-9" style="padding-bottom: 150px; padding-top: 30px; background-color: #EDF060; border-style: solid;">
+                <div class="col-md-9"
+                     style="padding-bottom: 150px; padding-top: 30px; background-color: #EDF060; border-style: solid;">
                     <h4>Seat status</h4>
                     <div class="form-group">
                         <label class="col-md-2 control-label" for="input5">Slot 1</label>
